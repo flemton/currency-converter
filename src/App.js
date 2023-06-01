ae9cd2660ba5529c6a16f616e2845e4db790388d
Osman Alhassan <oalhassan847@gmail.com>
2023-06-01 00:31:23 +0000

    Add Navbar and Layout.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
