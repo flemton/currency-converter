9f898071b1859f24ffae747637e4eb2e1aa6bca6
Osman Alhassan <oalhassan847@gmail.com>
2023-06-01 02:03:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import store from './redux/store';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route index element={<Home />} />
-        </Routes>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route index element={<Home />} />
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </div>
   );
