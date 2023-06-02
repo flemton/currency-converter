10eb812644af1626d6e50b78bcf8ad619d4ad4b9
Osman Alhassan <oalhassan847@gmail.com>
2023-06-02 09:38:17 +0000

    Remove back button from homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,6 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <nav className="nav">
     <NavLink to="/">
-      <h1 className="homepage">{'<'}</h1>
-    </NavLink>
-    <NavLink to="/">
       <h1 className="header">Currency Converter</h1>
     </NavLink>
   </nav>
